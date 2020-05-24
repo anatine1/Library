deaf46a8616be76662ecc9ecbf530d526adea928
anatine1 <63444049+anatine1@users.noreply.github.com>
2020-05-23 20:04:28 -0600

    Updated modal changed styling

--- libraryScript.js
+++ libraryScript.js
@@ -1,16 +1,9 @@
-let myLibrary =[];
-
-function addToLibrary(lordOfTheRings) {
-    var divBook = document.createElement("div");
-    divBook.className = 'book';
-    divBook.innerHTML = this.name, 
-                    this.author,
-                    this.pages;
-    document.body.appendChild(divBook);
 
 
-}
+const lordOfTheRings = new Book('Lord of the Rings', 'J.R.R Tolkien', '442');
+const TaleOfTwoCities = new Book('tale', 'charles dickens', '400');
 
+let myLibrary =[lordOfTheRings, TaleOfTwoCities];
 
 function Book(name, author, pages,) {
     this.name = name,
@@ -21,7 +14,51 @@ Book.prototype.info = function() {
         return(this.name);
 }
 
-const lordOfTheRings = new Book('Lord of the Rings', 'J.R.R Tolkien', '442');
+function render(Obj) {
+    var bookDiv = document.createElement('div')
+    bookDiv.className='book';
+    bookDiv.innerHTML = `<h1> Title: ${Obj.name}  </h1>` 
+    container.appendChild(bookDiv);
+}
+
+myLibrary.forEach(Book => { render(Book)});
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
+
+function addToLibrary() {
+    var titleAdd = document.getElementById('title').innerText;
+    var authorAdd = document.getElementById('author').innerText;
+    var pagesAdd = document.getElementById('pages').innerText;
+    var newBook = object.create(Book(titleAdd, authorAdd, pagesAdd));
+    myLibrary.push(newBook);
+}
+  
+
+
 
 
 
